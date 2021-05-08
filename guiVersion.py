62347a0fed62ac58c10693ebab139ed98b18990b
Damian Demiraj <58723742+ddemiraj@users.noreply.github.com>
2021-05-08 21:31:26 +0200

    Fixed Partlist and added Namesupport
    Partlist fixed
    Name will now be added to the Excel file if the user uses the MOC-Nr/Link for creating an Export

--- guiVersion.py
+++ guiVersion.py
@@ -19,7 +19,7 @@ import tkinter as tk
 ######################## Proxy ###############################################
 # Adapt the Proxy to youre infrastructure
 # If you make proxy = false proxyDict will be ignored
-proxy = True
+proxy = False
 http_proxy  = "http://uzu047.buhler-ltd.com:3128"
 https_proxy = "https://uzu047.buhler-ltd.com:3128"
 proxyDict = { 
@@ -38,8 +38,8 @@ if os.path.isdir(path) == False:
 # Adapt the variables
 mydb = mysql.connector.connect(
 host="localhost",
-user="webscrapper",
-password="webscrapper",
+user="root",
+password="",
 database="webscrapper"
 )
 database="webscrapper"
@@ -55,19 +55,47 @@ class AppURLopener(urllib.request.FancyURLopener):
 
 
 # Gets the html from the site
-def get_url(url):
+def get_html(url):
     try: 
         if (proxy == True):
             response = requests.get(url, proxies=proxyDict) 
         else:
-            response = requests.get(url) 
+            response = requests.get(url)
         content_type = response.headers['Content-Type'].lower()
-        if (response.status_code == 200 and content_type is not None 
-            and content_type.find('html') > -1): 
+        if (response.status_code == 200 and content_type is not None and content_type.find('html') > -1): 
           return response.content 
     except Exception as e: 
         print ('error when accessing', url, '\n', e) 
     return None
+
+def get_html_Login(url):
+    username = "dämler"
+    password = "BLJ4ever"
+    driver = webdriver.Chrome("C:/chromedriver.exe")
+    try: 
+            username = "dämler"
+            password = "Abc_1234"
+
+            loginPage = "https://rebrickable.com/login/"
+            driver = webdriver.Chrome("C:/chromedriver.exe")
+            driver.get(loginPage)
+            
+            user = driver.find_element_by_name("username")
+            user.send_keys(username)
+            
+            pas = driver.find_element_by_name("password")
+            pas.send_keys(password)
+
+            pas.submit()
+            driver.get(url)
+            html = driver.page_source
+            print(html)
+            driver.close()
+            driver.quit()
+    except Exception as e: 
+        print ('error when accessing', url, '\n', e) 
+        return None
+    return html
 
 # Select the elements needed and converts them into an array
 def extract_data(response_content): 
@@ -89,15 +117,18 @@ def split_data(inputArray, length):
 # Add color code to array
 def addColor_addImage(currentArray, response_content):
     rawData = BeautifulSoup(response_content, 'html.parser')
+    f = open("C:\Exports\debugOut.txt", "w")
+    f.write(str(rawData))
+    f.close()
     i = 0
     items = []
     for index, element in enumerate(rawData.select('img')):
          element = str(element)
          element = element.replace('<img class="img-responsive" data-src="', '')
+         element = element.replace('<img class="img-responsive lazy-hidden" data-src="', '')
          element = element.replace('" height="85" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" width="85"/>', '')
          urllib._urlopener = AppURLopener()
          urllib._urlopener.retrieve(element,"C:/Rebrickable_Pictures_temp/" + str(index) + ".jpg")
-         # urllib.request.urlretrieve(element, "C:/Rebrickable_Pictures_temp/" + str(index) + ".jpg")
          items.append(index)
     while i < len(currentArray):
         appendArray = currentArray[i]
@@ -111,50 +142,49 @@ def addColor_addImage(currentArray, response_content):
      
 
 def urlInput(urlInput):
-    driver = webdriver.Chrome("C:/xampp/chromedriver.exe")
-    # driver = webdriver.Chrome()
+    driver = webdriver.Chrome("C:/chromedriver.exe")
 	# Input is the userinput, output is the URL of the HTML-table or an error
     if("https://" in urlInput or "http://" in urlInput):
         # Get URL from argument
         URL = urlInput
+        inventoryPath = URL
     else:
         URL = str('https://rebrickable.com/mocs/'+urlInput)
 
-        # Replace after tailing slash
-        deleteParts = {'/#comments', '/#buy_parts', '/#bi', '/#photos', '/#parts'}
-        for delete in deleteParts:
-            URL = URL.replace(str(delete), '')
+    # Replace after tailing slash
+    deleteParts = {'/#comments', '/#buy_parts', '/#bi', '/#photos', '/#parts', '#details'}
+    for delete in deleteParts:
+        URL = URL.replace(str(delete), '')
+        
+    if(URL[len(URL) - 1] != '/'):
+        URL = URL + '/#parts'
             
-        if(URL[len(URL) - 1] != '/'):
-            URL = URL + '/#parts'
+    else:
+        URL = URL + '#parts'
             
-        else:
-            URL = URL + '#parts'
+    # retry while error occured
+    error = True  # only initial value
+    attempt = 0
+    while error and attempt < 3:
+        try:
+            print(URL)
+            driver.get(URL)
+            page = driver.page_source
+            soup = BeautifulSoup(page, features="html.parser")
+            results = soup.find(class_='btn-group js-export-parts-list')
+           
+            export_elements = results.find_all('li', class_='')
+            # The Link with the HTML Grid table
+            a = export_elements[4].find('a')
+            inventoryPath = 'https://rebrickable.com' + a['href']
+            error = False
+            break
             
-        # retry while error occured
-        error = True  # only initial value
-        attempt = 0
-        while error and attempt < 3:
-            try:
-                driver.get(URL)
-                page = driver.page_source
-
-                soup = BeautifulSoup(page, features="html.parser")
-                results = soup.find(class_='btn-group js-export-parts-list')
+        except:
+            error = True  # retry while loop
+            attempt += 1
                 
-                export_elements = results.find_all('li', class_='')
-
-                # The Link with the HTML Grid table
-                a = export_elements[4].find('a')
-                inventoryPath = 'https://rebrickable.com' + a['href']
-                error = False
-                break
-            
-            except:
-                error = True  # retry while loop
-                attempt += 1
-                
-                print("Attempting Retry " + str(attempt))
+            print("Attempting Retry " + str(attempt))
             
         # close selenium session
         driver.close()
@@ -165,36 +195,50 @@ def urlInput(urlInput):
 ######################### Input ##############################################
 ##############################################################################
 
-print("MOC-Nr.\t1\nMOC HTML-Table\t2\nPartlist HTML-Table\t3\n")
+print("MOC-Nr.\t1\nID of HTML-Table\t2\nPartlist HTML-Table\t3\n")
 typeInput = input()
 print("Type in the number or link")
 userInput = input()
 # MOC
+
+
+WorksheetName = "MOC-XXXXXX"
 if (int(typeInput) == 1):
-     txtOutput = "MOC"
+     WorksheetName = userInput
      if (userInput.startswith("MOC-") == False) and (userInput.startswith("https:") == False):
        userInput = "MOC-" + str(userInput)
+     else:
+         MocPositionStart = userInput.find("MOC-")
+         MocPositionStop = MocPositionStart+9
+         WorksheetName = userInput[MocPositionStart:MocPositionStop]
      userInput = urlInput(userInput)
 # MOC html table
 if (int(typeInput) == 2):
-     txtOutput = "MOC"
      if (userInput.startswith("https:") == False):
        userInput = "https://rebrickable.com/inventory/" + str(userInput) + "/parts/?format=table"
       
 # Part HTML table
 if (int(typeInput) == 3):
-    txtOutput = "Parttable" + userInput
-    if (urlInput.startswith("https:") == False):
+    if (userInput.startswith("https:") == False):
        userInput = "https://rebrickable.com/users/Exonic/partlists/" + str(userInput) + "/parts/?format=table"
-    
+
 url = userInput
+
+
+
 
 ##############################################################################
 ######################### Get and process data ###############################
 ##############################################################################
 
 # Gets the html from the site
-rawData = get_url(url)
+if(int(typeInput) == 3):
+    rawData = get_html_Login(url)    
+    print("get_html_Login")
+else:
+    rawData = get_html(url)
+    print("get_html")
+
 # Select the elements needed and converts them into an array
 processedData = extract_data(rawData)
 # Throws out \n\n in the table
@@ -202,6 +246,7 @@ processedData = list(filter(('\n\n').__ne__, processedData))
 # Splits the array into a 2-dimentional array with an array for every dataset
 processedData = split_data(processedData, 4)
 # Add color code to array
+print(processedData)
 processedData = addColor_addImage(processedData, rawData)
 
 ##############################################################################
@@ -253,8 +298,8 @@ for row in listAllParts:
 # Used to make a different Name and save
 now = datetime.now()
 current_time = now.strftime("%d_%m_%Y_%H.%M")
-workbook.save("C://Export_" + txtOutput + "_" + current_time + ".xlsx")
-
+workbook.save("C:/Exports/Export_" + WorksheetName + "_" + current_time + ".xlsx")
+print("Workbook saved")
 # Remove the Directory that was created for the Pictures
 # Caution, by changeing the path its possible to destroy youre os
 shutil.rmtree(path)
